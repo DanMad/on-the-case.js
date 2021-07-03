86fbeb0cfe8fcb541b8a9a956362f333e0b76ff1
Daniel Maddison <dmaddison@icloud.com>
2021-07-03 12:01:47 +1000

    Added toKebabCase

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-const str: string = 'hello world';
+import toKebabCase from "./to-kebab-case";
+
+if ('toKebabCase' in String) {
+  console.error(
+    'Error from onTheCase.js:\n' +
+    'toKebabCase could not be written to JavaScript\s String object because' +
+    'toKebabCase already exists. Consider using toKebabCase as a global' +
+    'function instead.'
+  );
+} else {
+  String.prototype.toKebabCase = toKebabCase;
+}
+
---
+++ types/string.d.ts
@@ -0,0 +1,3 @@
+declare interface String {
+  toKebabCase(string: string): string;
+}

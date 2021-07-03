a40a7c1ef7743692ace8ce77629c4ee332dacbc5
Daniel Maddison <dmaddison@icloud.com>
2021-07-03 13:59:59 +1000

    Ran prettier from root dir of project

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import toCamelCase from "./to-camel-case";
-import toKebabCase from "./to-kebab-case";
-import toPascalCase from "./to-pascal-case";
-import toSnakeCase from "./to-snake-case";
-import toTitleCase from "./to-title-case";
+import toCamelCase from './to-camel-case';
+import toKebabCase from './to-kebab-case';
+import toPascalCase from './to-pascal-case';
+import toSnakeCase from './to-snake-case';
+import toTitleCase from './to-title-case';
 
 const funcs: Record<string, (string: string) => string> = {
   toCamelCase,
@@ -10,17 +10,17 @@ const funcs: Record<string, (string: string) => string> = {
   toPascalCase,
   toSnakeCase,
   toTitleCase,
-}
+};
 
 Object.keys(funcs).map((func: string) => {
   if (`${func}` in String) {
     console.error(
       'Error from onTheCase.js:\n' +
-      `${func} could not be written to JavaScript\'s String object because` +
-      `${func} already exists. Consider using ${func} as a global` +
-      'function instead.'
+        `${func} could not be written to JavaScript\'s String object because` +
+        `${func} already exists. Consider using ${func} as a global` +
+        'function instead.',
     );
   } else {
     String.prototype[func] = func;
-  };
+  }
 });

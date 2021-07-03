6c087abca8082cd4d6b7475a4d3974a36f5824f9
Daniel Maddison <dmaddison@icloud.com>
2021-07-03 23:00:10 +1000

    Updated toTitleCase and its tests

--- src/to-title-case/index.test.ts
+++ src/to-title-case/index.test.ts
@@ -3,23 +3,7 @@ import toTitleCase from './index';
 global.String.prototype.toTitleCase = toTitleCase;
 
 describe('toTitleCase', () => {
-  it('Supports camel case transformation', () => {
-    expect(' fooBarBaz '.toTitleCase()).toBe(' Foo Bar Baz ');
-  });
-
-  it('Supports kebab case transformation', () => {
-    expect('  foo-bar-baz'.toTitleCase()).toBe('  Foo Bar Baz');
-  });
-
-  it('Supports pascal case transformation', () => {
-    expect('FooBarBaz  '.toTitleCase()).toBe('Foo Bar Baz  ');
-  });
-
   it('Supports sentence case transformation', () => {
-    expect('Foo bar  baz'.toTitleCase()).toBe('Foo Bar Baz');
-  });
-
-  it('Supports snake case transformation', () => {
-    expect('foo_bar_baz'.toTitleCase()).toBe('Foo Bar Baz');
+    expect('the foo bar And BAZ'.toTitleCase()).toBe('The Foo Bar and Baz');
   });
 });

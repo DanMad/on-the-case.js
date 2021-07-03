fe38f4e2440dfc651f4398ebe526a5b26a22c4bc
Daniel Maddison <dmaddison@icloud.com>
2021-07-03 13:59:34 +1000

    Added respective func dirs and test files

---
+++ src/to-kebab-case/index.ts
@@ -0,0 +1,7 @@
+const toKebabCase = (string: string): string =>
+  string
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+|\b)|[A-Z]?[a-z]+|[A-Z]|[0-9]+/g)
+    .map((word: string): string => word.toLowerCase())
+    .join('-');
+
+export { toKebabCase as default };

f0f3b7d1eaeebb62d02bd9ef2695d0e715d814c3
Daniel Maddison <dmaddison@icloud.com>
2021-07-03 18:22:29 +1000

    Updated toCamelCase and its tests

--- src/to-camel-case/index.ts
+++ src/to-camel-case/index.ts
@@ -1,7 +1,14 @@
-const toCamelCase = () => {
-  const string = String(this);
+function toCamelCase(): string {
+  const str = String(this);
 
-  return string;
-};
+  return str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+|\b)|[A-Z]?[a-z]+|[A-Z]|[0-9]+/g)
+    .map((subStr: string, i: number): string =>
+      i === 0
+        ? subStr.toLowerCase()
+        : subStr[0].toUpperCase() + subStr.substr(1).toLowerCase(),
+    )
+    .join('');
+}
 
 export { toCamelCase as default };

b010283746a675f1e0ccaeb6b6177d440ecb7b4f
Daniel Maddison <dmaddison@icloud.com>
2021-07-13 21:04:06 +1000

    Updated Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
   },
   resolve: {
     extensions: ['.ts'],
